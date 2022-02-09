6bba64428e1c791ffaa9f18c773aa92542cc8cc2
MatteoRossi01 <93929073+MatteoRossi01@users.noreply.github.com>
2022-02-09 19:12:11 +0100

    Esercizio completato

---
+++ main.js
@@ -0,0 +1,9 @@
+const root = new Vue (
+    {
+        el : "#root",
+        data: {
+            messaggio: "sonasona",
+            immagine: "https://www.sapevatelo.org/wp-content/uploads/2018/02/Fiori-33-800x670-700x586.jpg"
+        } 
+    }
+);
